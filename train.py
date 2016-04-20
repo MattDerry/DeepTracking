84714f85e8e3c470c34f4cf3721e1e5d85614474
MattDerry <mderry@gmail.com>
2016-04-20 18:03:25 -0400

    starting the training script, matching the command line arguments of the original lua code.

--- train.py
+++ train.py
@@ -14,6 +14,13 @@ def main():
     parser.add_argument('-d', '-data', default='data.t7', action='store', help='training data file')
     parser.add_argument('-lr', '-learningRate', default=0.01, type=float, action='store', help='learning rate')
     parser.add_argument('-iw', '-initweights', default='', action='store', help='initial weights from file')
+    parser.add_argument('-grid_minX', default=-25, type=float, action='store', help='occupancy grid bounds [m]')
+    parser.add_argument('-grid_maxX', default=25, type=float, action='store', help='occupancy grid bounds [m]')
+    parser.add_argument('-grid_minY', default=-45, type=float, action='store', help='occupancy grid bounds [m]')
+    parser.add_argument('-grid_maxY', default=5, type=float, action='store', help='occupancy grid bounds [m]')
+    parser.add_argument('-grid_step', default=1, type=float, action='store', help='resolution of the occupancy grid [m]')
+    parser.add_argument('-sensor_start', default=-180, type=float, action='store', help='first depth measurement [degrees]')
+    parser.add_argument('-sensor_step', default=0.5, type=float, action='store', help='resolution of depth measurements [degrees]')
     args = parser.parse_args()
     print args
 
